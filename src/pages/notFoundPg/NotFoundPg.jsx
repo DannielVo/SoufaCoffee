2772eb9c732b54b924fd0d5124755bd1e85a648f
DannielVo <phattinhoc2017@gmail.com>
2025-11-14 12:22:53 +0700

    Feat: Finish 404 Not found page

--- src/pages/notFoundPg/NotFoundPg.jsx
+++ src/pages/notFoundPg/NotFoundPg.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./notFoundPg.css";
+import { useNavigate } from "react-router-dom";
 
 const NotFoundPg = () => {
-  return <div>NotFoundPg</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="notfound-container">
+      <h1 className="notfound-title">404</h1>
+      <h2 className="notfound-content">Page not found!</h2>
+      <a onClick={() => navigate("/")} className="back-btn">
+        Back to Home Page
+      </a>
+    </div>
+  );
 };
 
 export default NotFoundPg;
