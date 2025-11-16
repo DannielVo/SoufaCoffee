af00349fef3345e9a38a2af6f08f6902c5bcb2ae
DannielVo <phattinhoc2017@gmail.com>
2025-11-16 20:35:07 +0700

    Feat: Finish Login page and import Main Logo

---
+++ src/assets/assets.js
@@ -0,0 +1,7 @@
+import mainLogo from "./Main_Logo.png";
+import mainLogo_3 from "./Main_Logo_3.png";
+
+export const assets = {
+  mainLogo,
+  mainLogo_3,
+};
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import "./login.css";
+import { assets } from "../../assets/assets";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login-wrapper">
+      <div className="login-container">
+        <div className="login-header">
+          <img src={assets.mainLogo_3} />
+        </div>
+
+        <form>
+          <div className="login-form-group">
+            <label htmlFor="">Email</label>
+            <input type="email" name="" id="" />
+          </div>
+
+          <div className="login-form-group">
+            <label htmlFor="">Password</label>
+            <input type="password" name="" id="" />
+          </div>
+
+          <div className="error-text">
+            "Oops! Something went wrong. Please try again later!"
+          </div>
+
+          <div className="login-form-footer">
+            <button className="login-btn" type="submit">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
