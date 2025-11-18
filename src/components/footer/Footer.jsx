cbd648e1b73451080f4fafe94811167caddb90e7
DannielVo <phattinhoc2017@gmail.com>
2025-11-18 23:07:51 +0700

    Feat: Finish footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./footer.css";
+import { assets } from "../../assets/assets";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer-wrapper">
+      <div className="footer-content">
+        <div className="footer-logo">
+          <img src={assets.mainLogo_3} alt="" />
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <p>© 2025 Soufa Coffee. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
