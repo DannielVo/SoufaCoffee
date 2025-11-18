08afc76e468e9a2a310bb042362da545d5569a46
DannielVo <phattinhoc2017@gmail.com>
2025-11-18 23:06:01 +0700

    Feat: Finish Profile page

--- src/pages/SettingPage.jsx
+++ src/pages/SettingPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Footer from "../components/footer/Footer";
-import Header from "../components/header/Header";
+import Header from "../components/cashierHeader/CashierHeader";
 
 const SettingPage = ({ children }) => {
   return (
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -2,7 +2,57 @@ import React from "react";
 import "./profile.css";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="profile-wrapper">
+      <div class="user-profile-container">
+        <div class="user-avatar">
+          <i class="bx bx-user"></i>
+        </div>
+        <div class="user-info">
+          <h2>Your Information</h2>
+          <form>
+            <div class="profile-form-group">
+              <label for="fullname">Full Name</label>
+              <input
+                type="text"
+                id="fullname"
+                name="fullname"
+                placeholder="Enter your full name"
+              />
+            </div>
+            <div class="profile-form-group">
+              <label for="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter your email"
+              />
+            </div>
+
+            <div class="profile-form-group">
+              <label for="phone">Phone Number</label>
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                placeholder="Enter your phone number"
+              />
+            </div>
+
+            <div class="profile-btn">
+              <button type="reset" class="cancel-btn">
+                Cancel
+              </button>
+              <button type="submit" class="update-btn">
+                Update
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
