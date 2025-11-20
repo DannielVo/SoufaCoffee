298fc94dc1e926700426f3bc78832036b9822986
DannielVo <phattinhoc2017@gmail.com>
2025-11-20 09:58:08 +0700

    Feat: Finish UI of Order Details page and add state in order list page

---
+++ src/pages/orderDetails/OrderDetails.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import "./orderDetails.css";
+import { ORDER_PAYMENT, TRANSACTIONS } from "../../assets/dummyDB";
+
+const OrderDetails = ({ order, onBack }) => {
+  const formatPrice = (price) => {
+    return new Intl.NumberFormat("vi-VN", {
+      style: "currency",
+      currency: "VND",
+      currencyDisplay: "code",
+    }).format(price);
+  };
+
+  return (
+    <div className="order-details-container left-right">
+      {/* ===== GENERAL INFO ===== */}
+      <div className="order-details-header">
+        <h3 className="section-title">General Information</h3>
+        <button className="btn-back" onClick={onBack}>
+          <i className="bx bx-arrow-back"></i>
+          Back
+        </button>
+      </div>
+
+      <section className="general-info">
+        <div className="info-grid">
+          <div className="info-item">
+            <span>Order:</span> <strong>#{order.id}</strong>
+          </div>
+
+          <div className="info-item">
+            <span>Staff:</span> <strong>{order.staff}</strong>
+          </div>
+
+          <div className="info-item">
+            <span>Date:</span> <strong>{order.created_at}</strong>
+          </div>
+
+          <div className="info-item">
+            <span>Total Amount:</span>{" "}
+            <strong>{formatPrice(order.total)}</strong>
+          </div>
+
+          <div className="info-item">
+            <span>Status: </span>
+
+            <span className={`status-badge ${order.status.toLowerCase()}`}>
+              {order.status}
+            </span>
+          </div>
+        </div>
+      </section>
+
+      {/* ===== ORDER ITEMS ===== */}
+      <section className="details-section">
+        <h3 className="section-title">Order Items</h3>
+
+        <div className="details-table">
+          <div className="table-header">
+            <div>Name</div>
+            <div>Quantity</div>
+            <div>Price</div>
+          </div>
+
+          {ORDER_PAYMENT.map((item) => (
+            <div className="table-row" key={`order-details${item.id}`}>
+              <div>{item.name}</div>
+              <div>{item.qty}</div>
+              <div>{formatPrice(item.price)}</div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* ===== TRANSACTION ===== */}
+      <section className="details-section">
+        <h3 className="section-title">Transaction History</h3>
+
+        <div className="details-table">
+          <div className="table-header">
+            <div>Payment Method</div>
+            <div>Cash Given</div>
+            <div>Amount</div>
+            <div>Status</div>
+            <div>Type</div>
+            <div>Date</div>
+          </div>
+
+          {TRANSACTIONS.map((t, index) => (
+            <div className="table-row" key={`transaction${t.id}`}>
+              <div>{t.method}</div>
+              <div>{t.cash_given ? formatPrice(t.cash_given) : "-"}</div>
+              <div>{formatPrice(t.amount)}</div>
+              <div className={`status-badge small ${t.status.toLowerCase()}`}>
+                {t.status}
+              </div>
+              <div>{t.type}</div>
+              <div>{t.created_at}</div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default OrderDetails;
--- src/pages/orderList/OrderList.jsx
+++ src/pages/orderList/OrderList.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./orderList.css";
 import { useNavigate } from "react-router-dom";
 import { ORDER_LIST } from "../../assets/dummyDB";
+import OrderDetails from "../orderDetails/OrderDetails";
 
 const OrderList = () => {
+  const [isOrderDetails, setIsOrderDetails] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState(null);
+
+  const handleSelectedOrder = (item) => {
+    setSelectedOrder(item);
+    setIsOrderDetails(true);
+  };
+
   const navigate = useNavigate();
 
   const formatPrice = (price) => {
@@ -15,47 +24,58 @@ const OrderList = () => {
   };
 
   return (
-    <div className="order-list-wrapper left-right">
-      <h2 className="order-list-title">Order List</h2>
+    <>
+      {isOrderDetails === false ? (
+        <div className="order-list-wrapper left-right">
+          <h2 className="order-list-title">Order List</h2>
 
-      <div className="order-list-container">
-        <div className="order-grid header">
-          <div>No.</div>
-          <div>Order ID</div>
-          <div>Total Amount</div>
-          <div>Date</div>
-          <div>Status</div>
-          <div className="actions-col">Actions</div>
-        </div>
+          <div className="order-list-container">
+            <div className="order-grid header">
+              <div>No.</div>
+              <div>Order ID</div>
+              <div>Total Amount</div>
+              <div>Date</div>
+              <div>Status</div>
+              <div className="actions-col">Actions</div>
+            </div>
 
-        {/* List items */}
-        {ORDER_LIST.map((order, index) => (
-          <div className="order-grid item" key={order.id}>
-            <div>{index + 1}</div>
-            <div>#{order.id}</div>
-            <div>{formatPrice(order.total)}</div>
-            <div>{order.date}</div>
-            <div className={`status ${order.status.toLowerCase()}`}>
-              {order.status}
-            </div>
-            <div className="actions">
-              <button
-                className="btn-icon detail"
-                onClick={() => navigate(`/orders/${order.id}`)}
-              >
-                <i className="bx bx-info-circle"></i>
-              </button>
-              <button
-                className="btn-icon edit"
-                onClick={() => navigate(`/orders/${order.id}/edit`)}
-              >
-                <i className="bx bx-edit"></i>
-              </button>
-            </div>
+            {/* List items */}
+            {ORDER_LIST.map((order, index) => (
+              <div className="order-grid item" key={order.id}>
+                <div>{index + 1}</div>
+                <div>#{order.id}</div>
+                <div>{formatPrice(order.total)}</div>
+                <div>{order.created_at}</div>
+                <div className={`status ${order.status.toLowerCase()}`}>
+                  {order.status}
+                </div>
+                <div className="actions">
+                  <button
+                    className="btn-icon detail"
+                    onClick={() => {
+                      handleSelectedOrder(order);
+                    }}
+                  >
+                    <i className="bxr  bx-eye"></i>
+                  </button>
+                  <button
+                    className="btn-icon edit"
+                    onClick={() => navigate(`/orders/${order.id}/edit`)}
+                  >
+                    <i className="bx bx-edit"></i>
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      ) : (
+        <OrderDetails
+          order={selectedOrder}
+          onBack={() => setIsOrderDetails(false)}
+        />
+      )}
+    </>
   );
 };
 
