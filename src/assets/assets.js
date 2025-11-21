b41d15350f2b1af08423d2d5bf2662ac453cacca
DannielVo <phattinhoc2017@gmail.com>
2025-11-21 12:28:05 +0700

    Feat: Finish Sidebar of Dashboard page

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -7,3 +7,46 @@ export const assets = {
 };
 
 export const ORDER_STATUS = ["Pending", "In-progress", "Done"];
+
+export const DASHBOARD_SIDEBAR_KEY = [
+  {
+    id: 1,
+    key: "Dashboard",
+    icon: "bxr  bx-home",
+  },
+  {
+    id: 2,
+    key: "Staff",
+    icon: "bxr  bx-user-circle",
+  },
+  {
+    id: 3,
+    key: "Product",
+    icon: "bxr  bx-shopping-bag-alt",
+  },
+  {
+    id: 4,
+    key: "Ingredient",
+    icon: "bxr  bx-cookie",
+  },
+  {
+    id: 5,
+    key: "Warehouse",
+    icon: "bxr  bx-warehouse",
+  },
+  {
+    id: 6,
+    key: "Recipe",
+    icon: "bxr  bx-chef-hat",
+  },
+  {
+    id: 7,
+    key: "Order",
+    icon: "bxr  bx-list-ul-square",
+  },
+  {
+    id: 8,
+    key: "Preparation",
+    icon: "bxr  bx-dropdown",
+  },
+];
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./dashboard.css";
+import WareHouse from "../../components/wareHouse/WareHouse";
+import RecipePg from "../recipePg/RecipePg";
+import { DASHBOARD_SIDEBAR_KEY } from "../../assets/assets";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [activeTab, setActiveTab] = useState(DASHBOARD_SIDEBAR_KEY.dashboard);
+
+  return (
+    <div className="dashboard-container">
+      <aside className="dashboard-sidebar">
+        {DASHBOARD_SIDEBAR_KEY.map((item, index) => (
+          <a
+            key={`dashboard-tab-${item.id}`}
+            className={`sidebar-item ${
+              activeTab === `${item.key}` ? "sidebar-item-active" : ""
+            }`}
+            onClick={() => setActiveTab(item.key)}
+          >
+            <i class={item.icon}></i>
+            <span>{item.key}</span>
+          </a>
+        ))}
+      </aside>
+
+      <main>
+        {/* <WareHouse isManager={true} /> */}
+        {/* <RecipePg isManager={true} /> */}
+      </main>
+    </div>
+  );
 };
 
 export default Dashboard;
